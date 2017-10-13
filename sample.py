f1bc9890be7480a977652fb1ea4043f37af6688f
kuesa <32714062+kuesa@users.noreply.github.com>
2017-10-12 22:02:12 -0700

    Adding sampling, Finished training
    The training script has been fixed so that everything actually works now. Essentially, the error function has been recreated to actually work. By just changing that accuracy went from 14% to 92%. Sampling has been added and synapse values are saved after training. No checkpoints yet. Alpha value has been tweaked to 0.5, which seems to provide the best accuracy so far. No biases yet. Might change squashing function to be something more exotic. Sampling currently goes through ten iterations with a blown-up image of the digit, preceded by the guess of the program.

---
+++ sample.py
@@ -0,0 +1,48 @@
+import numpy as np 
+import random
+from mnist import MNIST 
+import cv2
+
+def sigmoid(x, derivative=False):
+    if (derivative == True):
+        return x * (1 - x)
+    return 1 / (1 + np.exp(-x))
+
+def makeY(inp):
+	oof = np.zeros((1,10))
+	oof[0, inp] = 1
+	return oof
+
+#our lovely dataset
+mndata = MNIST('D:/Programming/ML/MNIST Network 2017/data/')
+images, labels = mndata.load_testing()
+
+#load synapse values from training
+syn0 = np.load('sv/syn0.npy')
+syn1 = np.load('sv/syn1.npy')
+
+#set output image size
+dst = np.array((280,280))
+
+#looping
+for j in range(10):
+	#define input and expected output
+	dex = random.randint(0, 9999)
+	X = np.atleast_2d(np.asarray(images[dex]) / 255)
+	y = makeY(labels[dex])
+
+	#forward propagation
+	l0 = X
+	l1 = sigmoid(np.dot(l0, syn0))
+	l2 = sigmoid(np.dot(l1, syn1))
+
+	guess = np.argmax(l2)
+
+	#ui stuff
+	print(guess)
+	src = np.reshape(X, (28,28))
+	dst = cv2.resize(src, (280,280))
+	
+	cv2.imshow('display', dst)
+	cv2.waitKey(0)
+	cv2.destroyAllWindows()
---
+++ train.py
@@ -0,0 +1,102 @@
+import numpy as np 
+import random
+import math
+from mnist import MNIST
+import cv2
+
+#def sigmoid(x, derivative=False):
+#    if (derivative == True):
+#        for i in range(0, len(x)):
+#            for k in range(len(x[i])):
+#                if x[i][k] > 0:
+#                    x[i][k] = 1
+#                else:
+#                    x[i][k] = 0
+#        return x
+#    for i in range(0, len(x)):
+#        for k in range(0, len(x[i])):
+#            if x[i][k] > 0:
+#                pass  # do nothing since it would be effectively replacing x with x
+#            else:
+#                x[i][k] = 0
+#    return x
+
+def sigmoid(x, derivative=False):
+    if (derivative == True):
+        return x * (1 - x)
+    return 1 / (1 + np.exp(-x))
+
+def makeY(inp):
+	oof = np.zeros((1,10))
+	oof[0, inp] = 1
+	return oof
+
+mndata = MNIST('D:/Programming/ML/MNIST Network 2017/data/')
+images, labels = mndata.load_training()
+
+np.random.seed(1)
+
+syn0 = 2*np.random.random((784, 16))-1
+syn1 = 2*np.random.random((16,10))-1
+
+alpha = 0.5
+
+right = 0
+wrong = 0
+
+#forward propagation
+for j in range(1000000):
+	dex = random.randint(0, 59999)
+	X = np.atleast_2d(np.asarray(images[dex]) / 255)
+	y = makeY(labels[dex])
+
+	l0 = X
+	l1 = sigmoid(np.dot(l0, syn0))
+	l2 = sigmoid(np.dot(l1, syn1))
+
+	guess = np.argmax(l2)
+	l2_error = l2 - y
+	#l2_error[0, guess] = l2_error[0, guess]
+	total_error = np.sum(l2_error)
+
+	#archaic error calculation
+	#for i in range(10):
+	#	if(np.argmax(l2) == i): 
+	#		total_error += (1-l2[i])
+	#		l2_error[i] = (1-l2[i])
+	#	else: 
+	#		total_error += l2[i]
+	#		l2_error[i] = l2[i]
+
+	#delta = error*deriv
+	l2_delta = l2_error * sigmoid(l2, True)
+	l1_error = l2_delta.dot(syn1.T)
+	l1_delta = l1_error * sigmoid(l1, True)
+
+	#l1 & l2_delta
+	syn1 -= (alpha * np.dot(l1.T, l2_delta))
+	syn0 -= (alpha * np.dot(l0.T, l1_delta))
+
+	#display image - DO NOT ENABLE THIS!!!!!!
+	#cv2.imshow('x', np.reshape(X, (28, 28)))
+	#cv2.destroyAllWindows()
+
+	if(guess == labels[dex]):
+		correct = "Yes."
+		right += 1
+	else:
+		correct = "No."
+		wrong += 1
+
+	#print("[Guess: " + str(guess) + "] [Answer: " + str(labels[dex]) + "] [Correct? : " + correct + "]")
+	#print("[Correct Answers: " + str(right) + "] [Incorrect Answers: " + str(wrong) + "]")
+	#print(total_error)
+	#print(l2_error[0,guess])
+	print(str((right / (right + wrong)) * 100))
+
+percentRight = (right / (right + wrong)) * 100
+print("Correct Answers: " + str(right))
+print("Incorrect Answers: " + str(wrong))
+print("Percentage of Correct Answers: " + str(percentRight) + "%")
+np.save('sv/syn0', syn0)
+np.save('sv/syn1', syn1)
